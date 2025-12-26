1ce83e6f874458450c86ac78ff30031c188ba6d2
G <71615719+gevans3000@users.noreply.github.com>
2025-12-26 13:10:26 -0500

    Add one-liner journal updater

---
+++ btc-update.ps1
@@ -0,0 +1,12 @@
+﻿param(
+  [Parameter(ValueFromRemainingArguments=$true)]
+  [string[]]$Args
+)
+
+$ErrorActionPreference="Stop"
+if (-not $Args -or $Args.Count -eq 0) {
+  Write-Host 'Usage: .\btc-update.ps1 "TP1 hit"'
+  exit 2
+}
+
+python .\scripts\update.py ($Args -join " ")
